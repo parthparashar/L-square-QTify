62276075f5171db0b37a40b786ca188e4e3d8d9a
parthparashar <parthparashar94@gmail.com>
2023-12-08 23:11:08 +0530

    tooltip

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
 import photos from './Images/card_img.svg';
 import './Card.css'
 function Card(props)
@@ -8,7 +10,9 @@ function Card(props)
             <div className="cwrapper">
                 <div className="card">
                     <div className="card_img">
+                    <Tooltip title="42 songs" arrow>
                         <img src={props.image} className="card_frame" alt="musicfileimage" />
+                    </Tooltip>
                     </div>
                     <div className="card_text">
                         <span className="card_pill">{props.follows + " Follows"}</span>
